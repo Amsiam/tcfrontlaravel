4d12eece3efa592fb8b2f354e6c208598bd63c14
Amsiam <amsiam990@gmail.com>
2024-06-05 19:19:01 +0600

    slide size

--- resources/views/livewire/slider-section.blade.php
+++ resources/views/livewire/slider-section.blade.php
@@ -1,9 +1,9 @@
-<section class="mx-3 h-52 lg:mx-14 my-2 lg:my-3">
+<section class="mx-3 h-60 lg:mx-14 my-2 lg:my-3">
     <div class="carousel w-full">
 
         @foreach ($this->sliders as $key => $slider)
         <div id="slide{{$key}}" class="carousel-item relative w-full">
-            <img class="w-full h-52" src="{{config("app.image_path")}}/{{$slider->image}}" class="w-full" />
+            <img class="w-full h-60" src="{{config("app.image_path")}}/{{$slider->image}}" class="w-full" />
             <div
                 class="absolute flex justify-between transform -translate-y-1/2 left-1 lg:left-5 right-1 lg:right-5 top-1/2">
 
